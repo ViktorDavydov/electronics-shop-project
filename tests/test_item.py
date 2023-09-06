0b596686577d1719729bb36e4bb369bb5abbae60
ViktorDavydov <davydov.viktor.andreevich@gmail.com>
2023-09-06 23:04:06 +0300

    first commit

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,12 @@
-"""Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+def test_Item():
+    """Тестирование класса Item"""
+    item1 = Item("Смартфон", 5, 10)
+    item2 = Item("Ноутбук", 0, 50000)
+    assert item1.calculate_total_price() == 50
+    assert item2.calculate_total_price() == 0
+    assert item1.apply_discount() is None
+    assert item1.name == "Смартфон"
