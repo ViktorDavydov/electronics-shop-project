3c00112b80ab09407d9a140bd10e76ba136017d8
ViktorDavydov <davydov.viktor.andreevich@gmail.com>
2023-09-07 09:23:10 +0300

    second fix commit

--- tests/test_item.py
+++ tests/test_item.py
@@ -3,10 +3,22 @@ from src.item import Item
 
 
 def test_Item():
-    """Тестирование класса Item"""
+    """Тестирование полей класса Item"""
+    item1 = Item("Плазма", 200000, 2)
+    assert item1.name == "Плазма"
+    assert item1.price == 200000
+    assert item1.quantity == 2
+
+
+def test_calculate_total_price():
+    """Тестирование метода расчета количества товара"""
     item1 = Item("Смартфон", 5, 10)
-    item2 = Item("Ноутбук", 0, 50000)
+    item2 = Item("Ноутбук", 2, 50000)
     assert item1.calculate_total_price() == 50
-    assert item2.calculate_total_price() == 0
+    assert item2.calculate_total_price() == 100000
+
+
+def test_apply_discount():
+    """Тестирование метода применения скидки возвращающий None"""
+    item1 = Item("Наушники", 15000, 80)
     assert item1.apply_discount() is None
-    assert item1.name == "Смартфон"
